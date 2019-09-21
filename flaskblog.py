1bd944ec052c662a1ab807e7dd58f82e9d8d5af2
Audrey Wanjiku <audreynjiraini@gmail.com>
2019-09-21 15:06:58 +0300

    add templates and routes for login and register pages

--- flaskblog.py
+++ flaskblog.py
@@ -1,5 +1,10 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, flash
+from forms import RegistrationForm, LoginForm
+
+
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '6f3d54ebc6ed040fdb08b8254d5c833e'
 
 posts = [
     {
@@ -26,6 +31,32 @@ def home():
 @app.route('/about')
 def about():
     return render_template('about.html', title = 'About')
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        
+        return redirect(url_for('home'))
+    
+    return render_template('register.html', title = 'Register', form = form)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            
+            return redirect(url_for('home'))
+        
+        else:
+            flash('Login Unsuccessful. Please check username and password', 'danger')
+    
+    return render_template('login.html', title = 'Login', form = form)
 
 
 
