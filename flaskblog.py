4d705010c727d170f9a254ca3f6237f69d6f2f00
Audrey Wanjiku <audreynjiraini@gmail.com>
2019-09-21 12:09:02 +0300

    add templates and a base template from which the templates will inherit. Incorporate bootstrap

--- flaskblog.py
+++ flaskblog.py
@@ -1,15 +1,31 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
+
+posts = [
+    {
+        'author': 'Audrey Njiraini',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'September 21, 2019'
+    },
+    {
+        'author': 'Kendall Njiraini',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'September 22, 2019'
+    }
+]
+
 
 @app.route('/')
 @app.route('/home')
 def home():
-    return '<h1>Home Page</h1>'
+    return render_template('home.html', posts = posts)
 
 
 @app.route('/about')
 def about():
-    return '<h1>About</h1'
+    return render_template('about.html', title = 'About')
 
 
 
