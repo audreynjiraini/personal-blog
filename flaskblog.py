f49d7025b4c6ec09e4a28bd22015cd686bac0ecb
Audrey Wanjiku <audreynjiraini@gmail.com>
2019-09-23 08:08:55 +0300

    change app structure

--- flaskblog.py
+++ flaskblog.py
@@ -1,10 +1,25 @@
 from flask import Flask, render_template, redirect, url_for, flash
 from forms import RegistrationForm, LoginForm
+from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = '6f3d54ebc6ed040fdb08b8254d5c833e'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpeg')
+    password = db.Column(db.String(60), nullable=False)
+    
+    
+    def  __repr__(self):
+        return f'User("{self.username}", "{self.email}", "{self.image_file}")'
+    
 
 posts = [
     {
