9a0397d874c74835d148f6614cd9f87a312ce66f
Audrey Wanjiku <audreynjiraini@gmail.com>
2019-09-23 15:52:22 +0300

    add more routes for main

--- app/main/views.py
+++ app/main/views.py
@@ -1,8 +1,11 @@
-from flask import Flask, render_template, redirect, url_for, flash
+from flask import Flask, render_template, redirect, url_for, flash, abort
 from .forms import BlogForm, CommentForm
 from flask_sqlalchemy import SQLAlchemy
 from flask import Blueprint
 from . import main
+from ..models import User, Blog, Comment, Role, Subscriber
+from flask_login import login_required, current_user
+from ..email import mail_message
 
 
 posts = [
@@ -22,14 +25,64 @@ posts = [
 
 
 
-@main.route('/')
-@main.route('/home')
+@main.route('/',methods=['GET','POST'])
+@main.route('/home',methods=['GET','POST'])
 def home():
-    return render_template('index.html', posts = posts)
+    
+    blogs = Blog.query.all()
+        
+    title = "Welcome to My Blog"
+
+    return render_template('index.html', posts = blogs)
 
 
-# @main.route('/about')
-# def about():
-#     return render_template('about.html', title = 'About')
+@main.route('/user/<uname>')
+@login_required
+def profile():
+    user = User.query.filter_by(username = uname).first()
+    
+    if user is None:
+        abort(404)
+    
+    return render_template('profile/profile.html', user = user)
 
 
+@main.route('/new_blog', methods=['GET','POST'])
+@login_required
+def new_post():
+    form = BlogForm()
+    if form.validate_on_submit():
+        title = form.title.data
+        content = form.content.data
+        category = form.category.data
+        new_post=Post(title=title,content=content,category=category)
+
+        new_post.save_post()
+
+    title="Make a blog post"
+    return render_template('new_blog.html',title=title,blog_form=form)
+
+
+@main.route("/post/<int:id>",methods=['GET','POST'])
+def blog(id):
+    blog = Blog.query.get_or_404(id)
+    comment = Comment.query.all()
+    form = CommentForm()
+
+    if request.args.get("like"):
+        post.like = post.like+1
+
+        db.session.add(post)
+        db.session.commit()
+
+        return redirect("/blog/{blog_id}".format(blog_id = blog.id))
+
+    if form.validate_on_submit():
+        comment = form.comment.data
+        new_comment = Comment(id=id,comment=comment,user_id=current_user.id,blog_id=blog.id)
+
+        new_comment.save_comment()
+
+        return redirect("/blog/{blog_id}".format(blog_id = blog.id))
+
+    return render_template('post.html',blog = blog,comments = comment,comment_form = form)
