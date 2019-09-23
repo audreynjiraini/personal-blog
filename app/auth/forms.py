629200afcbe94aaf68a8695d634e011d673b9ece
Audrey Wanjiku <audreynjiraini@gmail.com>
2019-09-23 09:40:05 +0300

    add validation to make sure username and email address on sign up are unique

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField, ValidationError
 from wtforms.validators import Required, Length, Email, EqualTo
+from ..models import User
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username', validators=[Required(), Length(min=2, max=20)])
@@ -12,6 +13,15 @@ class RegistrationForm(FlaskForm):
     confirm_password = PasswordField('Confirm Password', validators=[Required(), EqualTo('password')])
     
     submit = SubmitField('Sign Up')
+    
+    def validate_email(self, data_field):
+        if User.query.filter_by(email = data_field.data).first():
+            raise ValidationError('An account with that email already exists')
+        
+        
+    def validate_username(self, data_field):
+        if User.query.filter_by(username = data_field.data).first():
+            raise ValidationError('That username is taken')
     
     
     
